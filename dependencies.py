25e01ec3282003162a506e695b8250b1c214ef66
Renaolol <renanschmeier91@gmail.com>
2025-10-17 07:45:45 -0300

    Created exportador
    Criação do sistema de exportação para o sistema domínio. porém ainda não está baixando o arquivo .txt

--- dependencies.py
+++ dependencies.py
@@ -402,7 +402,21 @@ def processa_xml_nfse (xml):
     valor_pis = _parse_nfse_valor(root, "valor_pis")
     valor_cofins = _parse_nfse_valor(root, "valor_cofins")
     valor_issrf = _parse_nfse_valor(root,"valor_issrf")
+    data_emiss = root.find(".//data_nfse").text
+    cnpj = root.find(".//cpfcnpj").text
+    aliquota = root.find(".//aliquota_item_lista_servico").text
+    quantidade = root.find(".//unidade_quantidade").text
+    vlr_unit = root.find(".//unidade_valor_unitario").text
 
-    return numero,chave, valor_total, valor_desconto,valor_ir ,valor_inss, valor_contribuicao_social, valor_rps, valor_pis, valor_cofins, valor_issrf  
+    return numero, chave, valor_total, valor_desconto, valor_ir, valor_inss, valor_contribuicao_social, valor_rps, valor_pis, valor_cofins, valor_issrf , data_emiss, cnpj, aliquota, quantidade, vlr_unit 
+# FUNÇÃO PARA CRIAR O REGISTRO DE IMPORTAÇÃO DA DOMÍNIO PADRÃO COM SEPARADOR (REGISTRO 1000 NOTA FISCAL DE ENTRADA | REGISTRO PAI)
+def cria_registro_1000 (cnpj:int,num_doc:int,data_emissao,valor,chave):
+    return  f"|1000|39|{cnpj}||801|1933||{num_doc}|U||{data_emissao}|{data_emissao}|{valor}|||S||||||||||||||||||||||{valor}|0|||||||||||N||||||||||||||||||||||||||{chave}||||||||||||||||||||"
+# FUNÇÃO PARA CRIAR O REGISTRO DE IMPOSTOS (REGISTRO 1020 NOTA FISCAL DE ENTRADA - IMPOSTOS | REGISTRO FILHO)
+def cria_registro_1020(base_calculo:float,aliquota:float,valor:float):
+    return f"|1020|18||{base_calculo}|{aliquota}|{valor}||||||{base_calculo}|||||||||"
+# FUNÇÃO PARA CRIAR O REGISTRO DE ESTOQUE (REGISTRO 1030 NOTA FISCAL DE ENTRADA - ESTOQUE | REGISTRO FILHO)
+def cria_registro_1030(quantidade:int,vlr_unit:float,data_emissao,aliquota,vlor_issrf):
+    return f"|1030|557|{quantidade}|{vlr_unit}|||1|{data_emissao}||00|{vlr_unit}||||||||||||||||||||{aliquota}|{vlor_issrf}|1933||||||{vlr_unit}|||||||||||||||||||||||||||03||||||||||||||||||||||||||||||||||||"
 
 # ----------------------------
--- pages/Nfse.py
+++ pages/Nfse.py
@@ -1,16 +1,30 @@
 import streamlit as st
 import pandas as pd
-from dependencies import processa_xml_nfse,formata_valor
+from dependencies import processa_xml_nfse,formata_valor, cria_registro_1000, cria_registro_1020,cria_registro_1030
 from config_pag import set_background, get_ico, get_logo
+#Configurações da página
 set_background()
 get_logo()
 st.set_page_config("LEITOR XML NFSE",layout="wide",page_icon=get_ico())
 st.title("Leitor de XML de NFSE padrão nacional")
+
 xmls = st.file_uploader("Insira os XMLs",accept_multiple_files=True)
+#Criação da lista que será preenchida
 lista = []
 for xml in xmls:
     lista.append(processa_xml_nfse(xml))
-lista_df = pd.DataFrame(lista,columns=["Número","Chave","Valor Total","Desconto","IR","INSS","Contrib Social","RPS","PIS","COFINS","ISS RETIDO"])
+
+#Criação do DataFrame a partir da lista   
+lista_df = pd.DataFrame(lista,columns=["Número","Chave","Valor Total","Desconto","IR","INSS","Contrib Social","RPS","PIS",
+                                       "COFINS","ISS RETIDO","DATA","CNPJ EMISSOR","ALIQUOTA","QUANTIDADE","VALOR UNITARIO"])
 iss_retido = lista_df["ISS RETIDO"].sum()
 st.dataframe(lista_df) 
 st.metric("Soma de ISS Retido",formata_valor(iss_retido))
+
+exportar = st.button("Exportar Domínio")
+if exportar:
+    registro=[]
+    for x in lista:
+        registro.append([cria_registro_1000(x[12],x[0],x[11],x[2],x[1]),cria_registro_1020(x[2],x[13],x[10]),
+                         cria_registro_1030(x[14],x[15],x[12],x[13],x[10])])
+    st.write (registro)       
